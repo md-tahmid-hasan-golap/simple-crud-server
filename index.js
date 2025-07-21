a3c2af0c3bd84fa4bc639316cc98a84f6ce707af
md-tahmid-hasan-golap <golapraj47@gmail.com>
2025-07-21 16:20:03 +0600

    resolve

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const { MongoClient, ServerApiVersion } = require('mongodb');
 const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 3000;
+require('dotenv').config()
 
 app.use(cors());
 app.use(express.json());
@@ -10,7 +11,7 @@ app.use(express.json());
 //user : simpleDBuser
 //pass : vpLJKUVtJlJg9r1J
 
-const uri = "mongodb+srv://simpleDBuser:vpLJKUVtJlJg9r1J@cluster0.u8prwai.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const uri = `mongodb+srv://${process.env.db_user}:${process.env.db_password}@cluster0.u8prwai.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API //
 const client = new MongoClient(uri, {
@@ -27,21 +28,28 @@ async function run() {
 
     const database = client.db('usersdb');
     const userCollection = database.collection('users');
-  app.post('/users', async(req,res) => {
-    console.log('data in tha server',req.body)
-    const newUser = req.body;
-    const result = await userCollection.insertOne(newUser)
-    res.send(result)
-  })
 
+    app.get('/users', async (req, res) => {
+      const cursor = userCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
 
-    await client.db('admid').command( {ping: 1} )
-     console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    app.post('/users', async (req, res) => {
+      console.log('data in the server', req.body);
+      const newUser = req.body;
+      const result = await userCollection.insertOne(newUser);
+      res.send(result);
+    });
+
+    await client.db('admin').command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
-    // Ensures that the client will close when you finish/error
-    // await client.close();
+    // await client.close(); // এখন বন্ধ করিস না, নইলে ডেটাবেজ connect থাকবেনা
   }
 }
+
+
 run().catch(console.dir);
 
 app.get('/', (req,res) => {
